9d23f217e82a046e0e38af5557396d27f6a82f5c
Bcarz <bumpercar87@gmail.com>
2025-12-27 01:39:55 +0800

    FIRST COMMIT
    yipeeyipeeyipeeyipeeyipeee

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,176 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const DuckTapper());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DuckTapper extends StatelessWidget {
+  const DuckTapper({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ducky Quacker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Chelsea',
+        scaffoldBackgroundColor: Color(0xFF265490),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // Landing Page
+      home: LoginScreen(),
+
+      // Pages
+      routes: {
+        '/game': (context) => DuckScreen(),
+        '/upgrade': (context) => DuckScreen(),
+        '/data': (context) => DetailsScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// For Handling Data
+class DuckData{
+  String username = '';
+  int accountid = 0, duckid = 0;
+  int totalQuack = 0, currentQuack = 0, duckTaps = 0, moreDucks = 0, fish = 0, watermelon = 0, ponds = 0;
 
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>{
+  bool _passwordHide = true;
+
+  // Function to toggle the password visibility state.
+  void _togglePasswordVisibility() {
+    setState(() {
+      _passwordHide = !_passwordHide;
+    });
+  }
+
+  int test = 0;
+  void testNumber() async {
+    test++;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top:100),
+                width:165,
+                height:165,
+                child: Image.asset('assets/images/Duck.png', fit: BoxFit.contain),
+              ),
+              Text(
+                'Ducky Quacker',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 39),
+                
+              ),
+              Container(
+                margin: EdgeInsets.only(top:100),
+                width: 305,
+                height: 45,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFD9D9D9),
+                    hintText: 'Enter your username',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                )
+              ),
+              Container(
+                margin: EdgeInsets.only(top:20),
+                width: 305,
+                height: 45,
+                child: TextField(
+                  obscureText: _passwordHide,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFD9D9D9),
+                    hintText: 'Enter your Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        // Change the icon based on the state.
+                        _passwordHide ? Icons.visibility_off : Icons.visibility,
+                        semanticLabel: _passwordHide ? 'Hide password' : 'Show password',
+                      ),
+                      onPressed: _togglePasswordVisibility, // Call the toggle function on tap.
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                )
+              ),
+              Container(
+                margin: EdgeInsets.only(top:50),
+                child:ElevatedButton(
+                  onPressed: testNumber,
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(240, 50),
+                    backgroundColor: Color(0xFFCA8C35),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                )
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top:20),
+                child:ElevatedButton(
+                  onPressed: testNumber,
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(240, 50),
+                    backgroundColor: Color(0xFFCA8C35),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                )
+              )
+              
+            ]
+          )
+        )
+      )
+        
+    );
+  }
+}
+
+class DuckScreen extends StatefulWidget {
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -47,13 +180,11 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DuckState createState() => _DuckState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _DuckState extends State<DuckScreen> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -76,15 +207,79 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class DetailsScreen extends StatefulWidget {
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  _DetailState createState() => _DetailState();
+}
+
+class _DetailState extends State<DetailsScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
